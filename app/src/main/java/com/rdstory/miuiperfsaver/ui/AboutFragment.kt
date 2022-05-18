1555d2c86027c64b6e808c7a7d7ac4abf28b672b
test01wrk <test01wrk@163.com>
2022-05-18 10:09:17 +0800

    add a checkbox to hide launcher icon

--- app/src/main/java/com/rdstory/miuiperfsaver/ui/AboutFragment.kt
+++ app/src/main/java/com/rdstory/miuiperfsaver/ui/AboutFragment.kt
@@ -1,6 +1,8 @@
 package com.rdstory.miuiperfsaver.ui
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatCheckBox
 import androidx.fragment.app.Fragment
 import com.rdstory.miuiperfsaver.BuildConfig.VERSION_NAME
 import com.rdstory.miuiperfsaver.MainApplication
@@ -29,6 +32,17 @@ class AboutFragment : Fragment() {
                 )
             )
         }
+        val checkBox = root.findViewById<AppCompatCheckBox>(R.id.checkbox_show_icon)
+        val pm = checkBox.context.packageManager
+        val mainActivity = ComponentName(checkBox.context, MainActivity::class.java)
+        checkBox.setOnCheckedChangeListener { _, isChecked ->
+            pm.setComponentEnabledSetting(
+                mainActivity,
+                if (isChecked) PackageManager.COMPONENT_ENABLED_STATE_ENABLED else PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP
+            )
+        }
+        checkBox.isChecked = pm.getComponentEnabledSetting(mainActivity) != PackageManager.COMPONENT_ENABLED_STATE_DISABLED
         val version: TextView = root.findViewById(R.id.version)
         version.text = getString(R.string.app_version, VERSION_NAME)
         val xposed: Int = MainApplication.getActiveXposedVersion()
